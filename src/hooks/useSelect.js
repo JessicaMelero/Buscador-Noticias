4ff55dd1399e8df06f8f2ccfa10af4150d13273b
jessica <jessicameleromartinez@gmail.com>
2020-04-04 17:33:15 +0200

    Mostrando categorias de Select

--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -1,13 +1,19 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const useSelect = (stateInicial, opciones) => {
 
     // state del custom hook
-    const [state, actualizarState] = useState('');
-    
+    const [state, actualizarState] = useState(stateInicial);
+
     const SelectNoticias = () => (
-        <select className="browser-default">
-            <option value="">Seleccione</option>
+        <select 
+            className="browser-default"
+            value={state}
+            onChange={e => actualizarState(e.target.value)}
+        >
+            {opciones.map(option => (
+                <option key={option.value} value={option.value}>{option.label}</option>
+            ))}
         </select>
     );
 
