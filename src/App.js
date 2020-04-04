a1b0c548f03a4ebd714a82a865e9b5c81a744755
jessica <jessicameleromartinez@gmail.com>
2020-04-04 18:01:42 +0200

    Consultando API y leyendo resultados

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
@@ -7,14 +7,27 @@ function App() {
 
   // definir categorías y noticias
   const [categoria, guardarCategoria] = useState('');
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=d310417d4b1f49cd94ebff3c953ef888`;
+
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+
+      guardarNoticias(noticias.articles)
+    }
+    consultarAPI();
+  }, [categoria]);
 
   return (
     <Fragment>
-      <Header 
+      <Header
         titulo='Buscador de noticias'
       />
       <div className="container white">
-        <Formulario 
+        <Formulario
           guardarCategoria={guardarCategoria}
         />
       </div>
