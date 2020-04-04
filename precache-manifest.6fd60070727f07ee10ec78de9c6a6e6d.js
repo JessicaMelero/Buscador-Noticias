5defbeda8a5736ede143fa6bde7b754c062c65ae
jessica <jessicameleromartinez@gmail.com>
2020-04-04 18:52:24 +0200

    Updates

---
+++ precache-manifest.6fd60070727f07ee10ec78de9c6a6e6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5a3d87a90b651b520056ba54aaf232",
+    "url": "/JessicaMelero/BuscadorNoticias/index.html"
+  },
+  {
+    "revision": "24606ef86dab908bc8e0",
+    "url": "/JessicaMelero/BuscadorNoticias/static/css/main.eb8eb502.chunk.css"
+  },
+  {
+    "revision": "53b68bb7d2d1d8307c28",
+    "url": "/JessicaMelero/BuscadorNoticias/static/js/2.f36752ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JessicaMelero/BuscadorNoticias/static/js/2.f36752ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24606ef86dab908bc8e0",
+    "url": "/JessicaMelero/BuscadorNoticias/static/js/main.602ec703.chunk.js"
+  },
+  {
+    "revision": "71a6bc0f8186740d166b",
+    "url": "/JessicaMelero/BuscadorNoticias/static/js/runtime-main.85dd2e23.js"
+  }
+]);
